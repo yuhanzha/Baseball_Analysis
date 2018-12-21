138d5ea64c811b2e9d9e02d120ccd9161713b44d
yuhanzha <yz3284@columbia.edu>
2018-12-21 14:16:56 -0500

    Add files via upload

--- app/src/index.js
+++ app/src/index.js
@@ -1,1 +1,10 @@
+/* eslint-disable import/prefer-default-export */
+import EditableTable from './components/EditableTable.react';
+import VirtualizedTable from './components/VirtualizedTable.react';
+import DataTable from './components/DataTable.react';
 
+export {
+    DataTable,
+    EditableTable,
+    VirtualizedTable
+};
